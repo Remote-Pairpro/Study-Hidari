2aa693109af217f69944eef6f0929f2455816557
Hidari0415 <ika5140@gmail.com>
2016-01-19 23:26:59 +0900

    BetterListをTypeScript化

--- src/ts/better-list.ts
+++ src/ts/better-list.ts
@@ -1,23 +1,39 @@
 /// <reference path="tsd/typings/knockout/knockout.d.ts" />
-var BetterListModel = function() {
-    this.itemToAdd = ko.observable("");
-    this.allItems = ko.observableArray(["リコッタチーズ", "エシャロット", "ロマネスコ", "オリーブオイル", "イタリアンパセリ", "パルミジャーノチーズ"]);
-    this.selectedItems = ko.observableArray(["オリーブオイル"]);
- 
-    this.addItem = function() {
-        if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0))
-            this.allItems.push(this.itemToAdd());
-        this.itemToAdd("");
-    };
- 
-    this.removeSelected = function() {
+
+/**
+ * BetterList
+ */
+class BetterList {
+    constructor() {
+        this.itemToAdd = ko.observable("");
+        this.allItems = ko.observableArray(["リコッタチーズ", "エシャロット", "ロマネスコ", "オリーブオイル", "イタリアンパセリ", "パルミジャーノチーズ"]);
+        this.selectedItems = ko.observableArray(["オリーブオイル"]);
+    }
+    public itemToAdd : KnockoutObservable<string>;
+    public allItems : KnockoutObservableArray<string>;
+    public selectedItems : KnockoutObservableArray<string>;
+    
+    /**
+     * addItem
+     */
+    public addItem() : void {
+       if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0)){
+         this.allItems.push(this.itemToAdd());
+       }
+       
+       this.itemToAdd("");
+    }
+    
+    public removeSelected() : void {
         this.allItems.removeAll(this.selectedItems());
-        //this.selectedItems([]); // 選択状態をクリア
-    };
- 
-    this.sortItems = function() {
+    }
+    
+    /**
+     * sortItems
+     */
+    public sortItems() : void {
         this.allItems.sort();
-    };
-};
- 
-ko.applyBindings(new BetterListModel());
+    }
+}
+
+ko.applyBindings(new BetterList());
