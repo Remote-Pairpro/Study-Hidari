cc8ecddcbf872e4ec18b98425ed5f87044fdafca
Hidari0415 <ika5140@gmail.com>
2016-01-20 08:41:30 +0900

    TSでの.bind(this)の扱いがわからん...

--- src/ts/collection-sample.ts
+++ src/ts/collection-sample.ts
@@ -1,5 +1,27 @@
 /// <reference path="tsd/typings/knockout/knockout.d.ts" />
 
+/**
+ * PersonModel
+ * 
+ * 自分の名前と子を保持し、新たな子を追加するメソッドをもつ Person クラス。
+ */
+class PersonModel {
+    constructor(name: string, children:string[]) {
+        this.name = name;
+        this.children = ko.observableArray(children);
+    }
+    
+    public name: string;
+    public children: KnockoutObservableArray<string>;
+    
+    /**
+     * addChild
+     */
+    public addChild() {
+        this.children.push("New Child");
+    }
+}
+
 // 自分の名前と子を保持し、新たな子を追加するメソッドをもつ Person クラス
 var Person = function(name, children) {
     this.name = name;
