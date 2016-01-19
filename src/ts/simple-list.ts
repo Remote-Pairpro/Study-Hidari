0343ab1d09a28a8fcd116af40bd7686a9100e220
Hidari0415 <ika5140@gmail.com>
2016-01-19 12:05:33 +0900

    SimpleListをTypeScript化

---
+++ src/ts/simple-list.ts
@@ -0,0 +1,40 @@
+/// <reference path="tsd/typings/knockout/knockout.d.ts" />
+
+/**
+ * SimpleList
+ */
+class SimpleList {
+    constructor(data:string[]) {
+        this.items = ko.observableArray(data);
+        this.itemToAdd = ko.observable("");
+    }
+    public items : KnockoutObservableArray<string>;
+    public itemToAdd :  KnockoutObservable<string>;
+    
+    /**
+     * addItem
+     */
+    public addItem() {
+        if (this.itemToAdd() != "") {
+            this.items.push(this.itemToAdd());
+            this.itemToAdd("");
+        }
+    }
+}
+
+var SimpleListModel = function(items) {
+    this.items = ko.observableArray(items);
+    this.itemToAdd = ko.observable("");
+    this.addItem = function() {
+        if (this.itemToAdd() != "") {
+            // アイテムを追加します。
+            // 追加先の items は observableArray なので、対応する UI が更新されます。
+            this.items.push(this.itemToAdd());
+            // itemToAdd は Observable であり、テキストボックスにバインドされているため、
+            // 次のようにすることでテキストボックスをクリアできます。
+            this.itemToAdd("");
+        }
+    }.bind(this);  // this が常にこの ViewModel を指すようにします
+};
+
+ko.applyBindings(new SimpleList(["Alpha", "Beta", "Gamma"]));
