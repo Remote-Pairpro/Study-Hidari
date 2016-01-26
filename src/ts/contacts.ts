f60faad94db4bcdb7af3076ed7484237b7c144ae
Hidari0415 <ika5140@gmail.com>
2016-01-26 23:23:16 +0900

    remove系が上手く動かんけども＞＜

--- src/ts/contacts.ts
+++ src/ts/contacts.ts
@@ -1,3 +1,6 @@
+/// <reference path="tsd/typings/knockout/knockout.d.ts" />
+/// <reference path="tsd/typings/jquery/jquery.d.ts" />
+
 var initialData = [
     { firstName: "ダニー", lastName: "ラルッソ", phones: [
             { type: "携帯", number: "(555) 121-2121" },
@@ -14,21 +17,44 @@ var initialData = [
  * ContactsViewmodel
  */
 class ContactsViewmodel {
-    constructor() {
+    public contacts: KnockoutObservableArray<{firstName: any, lastName: any, phones: KnockoutObservableArray<{}>}>;
+    public lastSavedJson: KnockoutObservable<string>;
+    constructor(contacts) {
+        this.contacts = ko.observableArray(ko.utils.arrayMap(contacts, function(contact:{firstName:string; lastName:string; phones:any}) {
+            return { firstName: contact.firstName, lastName: contact.lastName, phones: ko.observableArray(contact.phones) };
+        }));
         
+        this.lastSavedJson = ko.observable("");
     }
     
-    public contacts: KnockoutObservableArray<{firstName: any, lastName: any, phones: KnockoutObservableArray<{}>}[]>;
-    
-    /**
-     * addContact
-     */
-    public addContact() {
+    public addContact():void{
         this.contacts.push({
-            firstName:"", lastName:"",phones:{}}[]);
+            firstName: "",
+            lastName:"",
+            phones: ko.observableArray()
+        })
+    }
+    
+    public removeContact(contact):void{
+        this.contacts.remove(contact);
+    }
+    
+    public addPhone(contact):void{
+        contact.phones.push({
+            type: "",
+            number: ""
+        })
+    }
+    
+    public removePhone(phone):void{
+        $.each(this.contacts(), function() { this.phones.remove(phone) });
+    }
+    
+    public save(){
+        this.lastSavedJson(JSON.stringify(ko.toJS(this.contacts), null, 2));
     }
 }
-
+/*
 var ContactsModel = function(contacts) {
     var self = this;
     self.contacts = ko.observableArray(ko.utils.arrayMap(contacts, function(contact) {
@@ -64,5 +90,5 @@ var ContactsModel = function(contacts) {
  
     self.lastSavedJson = ko.observable("")
 };
- 
-ko.applyBindings(new ContactsModel(initialData));
+ */
+ko.applyBindings(new ContactsViewmodel(initialData));
